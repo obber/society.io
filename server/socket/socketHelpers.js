7f21373efa046c35922658e464208f147ea65bea
Kan Adachi <kadachi1023@gmail.com>
2016-12-15 11:15:51 -0800

    removes auth 2

--- server/socket/socketHelpers.js
+++ server/socket/socketHelpers.js
@@ -2,16 +2,21 @@ var firebase = require('../common').firebase;
 
 function socketCheck(token, socket) {
 	return new Promise(function(resolve, reject) {
-		firebase.authWithCustomToken(token, function(err, authData)  {
-			if (err) {
-				reject(err);
-			} else {
-				resolve({
-					uid: authData.auth.uid,
-					socket: socket,
-					token: token
-				});
-			}
+		// firebase.authWithCustomToken(token, function(err, authData)  {
+		// 	if (err) {
+		// 		reject(err);
+		// 	} else {
+		// 		resolve({
+		// 			uid: authData.auth.uid,
+		// 			socket: socket,
+		// 			token: token
+		// 		});
+		// 	}
+		// });
+		resolve({
+			uid: '1',
+			socket: socket,
+			token: token
 		});
 	});
 }
